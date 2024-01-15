ebbfc14badd329b191c38c5a4727a8ff0236ddf8
Erdum <erdumadnan@gmail.com>
2024-01-15 21:48:11 +0500

    Refactor: firstOrFail method used instead of custom exception handling

--- app/Http/Controllers/DiscussionController.php
+++ app/Http/Controllers/DiscussionController.php
@@ -11,9 +11,7 @@ class DiscussionController extends Controller
 {
     public function get_discussion($id)
     {
-        $discussion = Discussion::find($id);
-
-        if (empty($discussion)) abort(404);
+        $discussion = Discussion::findOrFail($id);
 
         return view('discussion.index', ['discussion' => $discussion]);
     }
@@ -27,9 +25,7 @@ class DiscussionController extends Controller
 
     public function up_vote($discussion_id, Request $request)
     {
-        $discussion = Discussion::find($discussion_id);
-
-        if (empty($discussion)) abort(404);
+        $discussion = Discussion::findOrFail($discussion_id);
 
         $up_vote = new DiscussionUpVote;
         $up_vote->user_id = $request->user()->id;
@@ -40,9 +36,7 @@ class DiscussionController extends Controller
 
     public function delete_up_vote($discussion_id, Request $request)
     {
-        $discussion = Discussion::find($discussion_id);
-
-        if (empty($discussion)) abort(404);
+        $discussion = Discussion::findOrFail($discussion_id);
 
         $discussion->up_votes()->where('user_id', $request->user()->id)
             ->delete();
@@ -52,9 +46,7 @@ class DiscussionController extends Controller
 
     public function down_vote($discussion_id, Request $request)
     {
-        $discussion = Discussion::find($discussion_id);
-
-        if (empty($discussion)) abort(404);
+        $discussion = Discussion::findOrFail($discussion_id);
 
         $down_vote = new DiscussionDownVote;
         $down_vote->user_id = $request->user()->id;
@@ -65,10 +57,7 @@ class DiscussionController extends Controller
 
     public function delete_down_vote($discussion_id, Request $request)
     {
-        $discussion = Discussion::find($discussion_id);
-
-        if (empty($discussion)) abort(404);
-
+        $discussion = Discussion::findOrFail($discussion_id);
         $discussion->down_votes()->where('user_id', $request->user()->id)
             ->delete();
 
