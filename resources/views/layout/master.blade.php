7714979e882e8c9d05190893116eb00b0aba6007
Erdum <erdumadnan@gmail.com>
2024-02-08 21:23:57 +0500

    feat: error toast implemented for global errors

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -133,6 +133,50 @@
       </div>
     </div>
   </footer>
+
+  <!-- Global notification live region, render this permanently at the end of the document -->
+  <div aria-live="assertive" class="fixed inset-0 flex items-end px-4 py-6 pointer-events-none sm:p-6 sm:items-start">
+    <div class="w-full flex flex-col items-center space-y-4 sm:items-end">
+      <!--
+        Notification panel, dynamically insert this into the live region when it needs to be displayed
+
+        Entering: "transform ease-out duration-300 transition"
+          From: "translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+          To: "translate-y-0 opacity-100 sm:translate-x-0"
+        Leaving: "transition ease-in duration-100"
+          From: "opacity-100"
+          To: "opacity-0"
+      -->
+      @foreach ($errors->all() as $error)
+        <div onclick="closeErrorToast(this, event)" class="max-w-sm w-full bg-red-200 shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden transition ease-in duration-100">
+          <div class="p-4">
+            <div class="flex items-start">
+              <div class="flex-shrink-0">
+                <svg class="h-6 w-6 text-red-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+              </div>
+              <div class="ml-3 w-0 flex-1 pt-0.5">
+                <p class="text-sm font-medium text-black">Error!</p>
+                <p class="mt-1 text-sm text-gray-800">{{ $error }}</p>
+              </div>
+              <div class="ml-4 flex-shrink-0 flex">
+                <button class="rounded-md inline-flex text-gray-700 hover:text-gray-500 focus:outline-none">
+                  <span class="sr-only">Close</span>
+                  <!-- Heroicon name: solid/x -->
+                  <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      @endforeach
+
+    </div>
+  </div>
+
   <script>
 
     document.addEventListener('keydown', function (event) {
@@ -150,6 +194,17 @@
       'blur',
       () => closeTopicPopover()
     );
+
+    function closeErrorToast(elem, event) {
+
+      if (
+        Array.from(event.composedPath()).map(item => item.tagName)
+          .includes('BUTTON')
+      ) {
+        elem.classList.add('opacity-0', 'translate-y-2');
+        setTimeout(() => elem.remove(), 200);
+      }
+    };
 
     function openDiscussionWritingModal(event) {
       event.preventDefault();
