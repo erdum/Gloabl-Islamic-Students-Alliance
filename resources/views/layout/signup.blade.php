24ed23b6cf3271a9281cf1256c5e77d5fb76b804
Erdum <erdumadnan@gmail.com>
2024-02-10 19:00:29 +0500

    feat: user registration view implemented

---
+++ resources/views/layout/signup.blade.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html class="h-full bg-white">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+  @vite('resources/css/app.css')
+</head>
+
+<body class="h-full">
+  <div class="min-h-full flex">
+    <div class="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+      <div class="mx-auto w-full max-w-sm lg:w-96">
+        <div>
+          <!-- <img class="h-14 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-stone-600.svg" alt="Workflow"> -->
+          <h2 class="mt-6 text-3xl font-extrabold text-stone-700">Global Islamic Students Alliance</h2>
+          <p class="mt-2 text-sm text-gray-600">
+            Register your account
+            <!-- <a href="#" class="font-medium text-stone-600 hover:text-stone-500"> start your 14-day free trial </a> -->
+          </p>
+        </div>
+        <div class="mt-8">
+          <div>
+            <div>
+              <p class="text-sm font-medium text-gray-700">Register with</p>
+              <div class="mt-1 grid grid-cols-3 gap-3">
+                <div>
+                  <a href="#" class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
+                    <span class="sr-only">Sign in with Facebook</span>
+                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                      <path fill-rule="evenodd" d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z" clip-rule="evenodd" />
+                    </svg>
+                  </a>
+                </div>
+                <div>
+                  <a href="#" class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
+                    <span class="sr-only">Sign in with Twitter</span>
+                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                      <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                    </svg>
+                  </a>
+                </div>
+                <div>
+                  <a href="#" class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
+                    <span class="sr-only">Sign in with GitHub</span>
+                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                      <path fill-rule="evenodd" d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z" clip-rule="evenodd" />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="mt-6 relative">
+              <div class="absolute inset-0 flex items-center" aria-hidden="true">
+                <div class="w-full border-t border-gray-300"></div>
+              </div>
+              <div class="relative flex justify-center text-sm">
+                <span class="px-2 bg-white text-gray-500"> Or continue with </span>
+              </div>
+            </div>
+          </div>
+          <div class="mt-6">
+            <form enctype="multipart/form-data" action="{{ route('handle-signup') }}" method="POST" class="space-y-6">
+              @csrf
+
+              <div class="mt-1 sm:mt-0 sm:col-span-2">
+                <div class="flex items-center">
+                  <span class="h-12 w-12 rounded-full overflow-hidden bg-gray-100">
+                    <svg class="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
+                      <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                    </svg>
+                  </span>
+                  <button type="button" class="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-stone-500">Upload</button>
+                </div>
+              </div>
+
+              <div>
+                <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+                <div class="mt-1">
+                  <input id="name" name="name" type="text" autocomplete="name" required value="{{ old('name') }}" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-stone-500 focus:border-stone-500 sm:text-sm">
+                </div>
+              </div>
+
+              @if ($errors->has('email'))
+              <div>
+                <label for="email" class="block text-sm font-medium text-gray-700">Email address</label>
+                <div class="mt-1 relative rounded-md shadow-sm">
+                  <input id="email" name="email" type="email" autocomplete="email" required value="{{ old('email') }}" class="block w-full pr-10 border-red-300 text-red-900 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm rounded-md" aria-invalid="true" aria-describedby="name-error">
+                  <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                    <!-- Heroicon name: solid/exclamation-circle -->
+                    <svg class="h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+                    </svg>
+                  </div>
+                </div>
+                <p class="mt-2 text-sm text-red-600" id="email-error">
+                  {{ $errors->first('email') }}
+                </p>
+              </div>
+              @else
+              <div>
+                <label for="email" class="block text-sm font-medium text-gray-700">Email address</label>
+                <div class="mt-1">
+                  <input id="email" name="email" type="email" autocomplete="email" required value="{{ old('email') }}" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-stone-500 focus:border-stone-500 sm:text-sm">
+                </div>
+              </div>
+              @endif
+
+              @if ($errors->has('password'))
+              <div>
+                <label for="password" class="block text-sm font-medium text-gray-700">Email address</label>
+                <div class="mt-1 relative rounded-md shadow-sm">
+                  <input id="password" name="password" type="password" autocomplete="password" required value="{{ old('password') }}" class="block w-full pr-10 border-red-300 text-red-900 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm rounded-md" aria-invalid="true" aria-describedby="name-error">
+                  <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                    <!-- Heroicon name: solid/exclamation-circle -->
+                    <svg class="h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+                    </svg>
+                  </div>
+                </div>
+                <p class="mt-2 text-sm text-red-600" id="password-error">
+                  {{ $errors->first('password') }}
+                </p>
+              </div>
+              @else
+              <div class="space-y-1">
+                <label for="password" class="block text-sm font-medium text-gray-700"> Password </label>
+                <div class="mt-1">
+                  <input id="password" name="password" type="password" autocomplete="current-password" required value="{{ old('password') }}" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-stone-500 focus:border-stone-500 sm:text-sm">
+                </div>
+              </div>
+              @endif
+
+              <fieldset class="mt-6">
+                <div>
+                  <legend class="text-sm font-medium text-gray-700">
+                    Gender
+                  </legend>
+                </div>
+                <div class="mt-3 space-y-2">
+                  <div class="flex items-center">
+                    <input {{ old('gender') == 'male' ? 'checked' : '' }} required id="push-everything" name="gender" value="male" type="radio" class="focus:ring-stone-500 h-4 w-4 text-stone-600 border-gray-300">
+                    <label for="push-everything" class="ml-3 block text-sm font-medium text-gray-700">Male</label>
+                  </div>
+                  <div class="flex items-center">
+                    <input {{ old('gender') == 'female' ? 'checked' : '' }} required id="push-email" name="gender" value="female" type="radio" class="focus:ring-stone-500 h-4 w-4 text-stone-600 border-gray-300">
+                    <label for="push-email" class="ml-3 block text-sm font-medium text-gray-700">Female</label>
+                  </div>
+                </div>
+              </fieldset>
+
+              <div>
+                <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-stone-600 hover:bg-stone-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-stone-500">Register and Unite</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="hidden lg:block relative w-0 flex-1">
+      <img class="transition-opacity duration-1000 ease-in-out absolute inset-0 h-full w-full object-cover animate-pulse" src="{{ asset('images/fm-2-small.jpg') }}" alt="">
+      <img id="wallpaper" class="transition-opacity duration-1000 ease-in-out opacity-0 absolute inset-0 h-full w-full object-cover" src="{{ asset('images/fm-2.jpg') }}" loading="lazy" alt="">
+    </div>
+  </div>
+  <script>
+  const wallpaper = document.getElementById('wallpaper');
+
+  if (wallpaper.complete) {
+    loaded();
+  } else {
+    wallpaper.addEventListener('load', loaded);
+  }
+
+  function loaded() {
+    const overlay = wallpaper.previousElementSibling;
+    wallpaper.classList.remove('opacity-0');
+    overlay.classList.add('opacity-0');
+  }
+
+  </script>
+</body>
+
+</html>
