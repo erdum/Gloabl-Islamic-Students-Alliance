4955ae03d1816eb11dc26815c01a4fb705718559
Erdum <erdumadnan@gmail.com>
2024-02-11 13:37:14 +0500

    feat(UX): Home link attached to the app name in the app-bar

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -13,7 +13,11 @@
   <div class="bg-white pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
     <div class="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
       <div>
-        <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Global Islamic Students Alliance</h2>
+        <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
+          <a href="{{ route('home') }}">
+            Global Islamic Students Alliance
+          </a>
+        </h2>
         <div class="mt-3 sm:mt-4 lg:grid lg:grid-cols-2 lg:gap-5 lg:items-center">
           <p class="text-xl text-gray-500">
             Unite beyond borders, sects, ideologies, and identities. Welcome to a global sanctuary for Islamic intellectual minds, unite and stand for the true cause.
